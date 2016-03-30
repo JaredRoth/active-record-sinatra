62fd4dec0c94fbe403b50f9445dabe836346bdeb
Jared Roth <jared.roth@gmail.com>
2016-03-30 09:09:06 -0600

    Prepares for Wednesdat lesson

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,5 +1,16 @@
 class FilmFile < Sinatra::Base
   get '/films' do
+    @films = Film.all
     erb :films_index
+  end
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :genres_index
+  end
+
+  get '/director/:id' do |id|
+    @director = Director.find(id)
+    erb :show_director
   end
 end
